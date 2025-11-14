7f6dca1f6cfd0deb35b5de780a0f7d2a81e58533
Tower\dofiy <zdoristup@gmail.com>
2025-11-14 11:24:19 +0800

    去除生成文件、仅仅保留环境、代码-UTF8(BOM)编码

---
+++ Env/include/dicom/version.h
@@ -0,0 +1,24 @@
+#ifndef DCM_VERSION_H
+#define DCM_VERSION_H
+
+/* The project version.
+ */
+#define DCM_VERSION            "1.2.0"
+#define DCM_SUFFIXED_VERSION   "1.2.0"
+#define DCM_VERSION_MAJOR      (1)
+#define DCM_VERSION_MINOR      (2)
+#define DCM_VERSION_MICRO      (0)
+
+/* The ABI version, as used for library versioning.
+ */
+#define DCM_ABI_VERSION_MAJOR  (1)
+#define DCM_ABI_VERSION_MINOR  (2)
+#define DCM_ABI_VERSION_PATCH  (0)
+
+/* Whether libdicom is built statically.
+ */
+#define DCM_STATIC             (0)
+
+#endif /*DCM_VERSION_H*/
+
+
